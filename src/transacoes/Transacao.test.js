3aaac34912ad7a2c4ea73ae7fafdad23495cd0d2
velleda <81834718+velleeda@users.noreply.github.com>
2021-05-22 17:16:21 -0300

    first commit

---
+++ src/transacoes/Transacao.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Transacao from "./Transacao";
+
+describe("Componente de transação do extrato", () => {
+  it("O snapshot do componente sempre deve permanecer o mesmo", () => {
+    const { container } = render(
+      <Transacao data="08/09/2020" tipo="saque" valor="20.00" />
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
